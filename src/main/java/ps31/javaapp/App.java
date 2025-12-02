2585f3a6be0b0808a1bd870f272d80637e02e7a9
GotKarthik <gotvelocity@gmail.com>
2025-12-02 23:01:05 +0530

    message

---
+++ src/main/java/ps31/javaapp/App.java
@@ -0,0 +1,13 @@
+package ps31.javaapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello se lab!" );
+    }
+}
